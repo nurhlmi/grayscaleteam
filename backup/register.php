ef5b6bb50b1f2f44f77938e476bc81558d75e777
Hilmi <nurhilmi.mail@gmail.com>
2020-08-04 16:07:59 +0700

    first commit

---
+++ backup/register.php
@@ -0,0 +1,42 @@
+<!Doctype HTML>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
+		<meta name="description" content="Grayscale merupakan sebuah Tim kecil" />
+		
+		<title>Grayscale - Daftar</title>
+		
+		<link href="dist/css/sb-admin-2.min.css" rel="stylesheet" type="text/css">
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+		<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+		
+		<link rel="shorcut icon" href="img/icon.ico">
+		<link href="style.css" rel="stylesheet" type="text/css">
+	</head>
+	
+	<body style="background:black">
+		<div class="container">
+			<center>
+				<div class="form-login">
+					<img src="img/icon.ico" class="logo-login text-center" align="center">
+					<h3>Daftar</h3>
+					<hr>
+					<input name="nama" type="text" class="input" placeholder="Nama Lengkap" required>
+					<input name="email" type="email" class="input" placeholder="Email" autocomplete="off" required>
+					<input name="password" type="password" class="input" placeholder="Kata Sandi" required>
+					<input name="repassword" type="password" class="input" placeholder="Ulang Kata Sandi" required>
+					<button class="button-login btn btn-success">Daftar</button>
+					<div class="forget">
+						<a href="login" class="forget">Masuk</a> | 
+						<a href="forgot" class="forget">Lupa Kata Sandi</a>
+					</div>
+				</div>
+				<div class="footer-form text-center">
+					&copy; Grayscale Team 2018
+				</div>
+			</center>
+		</div>
+	</body>
+</html>
